04534a7d83525b3a2c5f152b8f7121388eff6f6b
Aad Versteden <madnificent@gmail.com>
2018-09-24 21:58:29 +0200

    Set bound method through publicDom decorator
    The publicDom decorator did not correctly set the public methods on
    the DOM element through an oversight.  This has been altered.

--- addon/decorators.js
+++ addon/decorators.js
@@ -14,6 +14,7 @@ export function publicDom(target) {
           let args = arguments;
           return run( () => functor.bind(self)(...args) );
         };
+        this.element[key] = newMethod;
       }
     }
   };
